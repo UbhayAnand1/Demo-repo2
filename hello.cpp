d8ee0cd9baf870fe58596f58d5927fbd754d5c48
UbhayAnand1 <100370172+UbhayAnand1@users.noreply.github.com>
2023-04-20 00:50:35 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int factorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
 int main() {
-    cout << "Hello World!";
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    cout << "Factorial of " << n << " = " << factorial(n);
     return 0;
 }
